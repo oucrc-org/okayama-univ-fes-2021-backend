fe8093e4b9b485025418a48fac773a4ac063e03d
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-10-24 16:16:11 +0900

    [add] question関連のテーブルのfactoryを追加

---
+++ database/factories/ClubFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Club;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ClubFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Club::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'stamp_image_path' => $this->faker->imageUrl,
+        ];
+    }
+}
