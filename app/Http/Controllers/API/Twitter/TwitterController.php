356f56a7069c210a7be43861cf4f9d1acd60c300
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-10-27 22:54:31 +0900

    [add] Twitterから取ってくる奴を追加

---
+++ app/Http/Controllers/API/Twitter/TwitterController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\API\Twitter;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class TwitterController extends Controller
+{
+    private $mPost;
+
+    function __construct(Post $post)
+    {
+        $this->mPost = $post;
+    }
+
+    public function get(): JsonResponse
+    {
+        $posts = $this->mPost->query()
+            ->where('is_visible', true)
+            ->get()
+            ->only(['twitter_id', 'avatar_url', 'display_name', 'comment', 'tweet_url']);
+
+        return response()->json(['success' => 'true', 'data' => $posts]);
+    }
+}
