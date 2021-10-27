6da55850904caf5f7692c5b43507b0d2de9fe798
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-10-27 22:49:04 +0900

    [fix] jsonで返す値を修正

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -4,30 +4,48 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
 
+    private $mUser;
+
+    function __construct(User $user)
+    {
+        $this->mUser = $user;
+    }
+
     function get(): JsonResponse
     {
-        //ユーザー情報
-        $user = Auth::user();
+        try
+        {
+            //ユーザー情報
+            $user_id = auth()->id();
 
-        /** @noinspection PhpPossiblePolymorphicInvocationInspection */
-        //スタンプ情報
-        $stamps = $user->questions()
-            ->select('stamp_id')
-            ->with('stamp')
-            ->get()->map(function ($item){
-                return $item->stamp->only(['id', 'name','image_path']);
-            });
+            $user = $this->mUser->query()
+                ->findOrFail($user_id);
 
-        return response()->json([
-            'success' => true,
-            'user' => $user,
-            'stamps' => $stamps
-        ]);
+            /** @noinspection PhpPossiblePolymorphicInvocationInspection */
+            //スタンプ情報
+            $stamps = $user->questions()
+                ->select('stamp_id')
+                ->with('stamp')
+                ->get()->map(function ($item){
+                    return $item->stamp->only(['id', 'name','image_path']);
+                });
+
+            return response()->json([
+                'success' => true,
+                'user' => $user->only(['google_id', 'display_name', 'email', 'avatar_url']),
+                'stamps' => $stamps
+            ]);
+        }
+        catch (ModelNotFoundException $e)
+        {
+            return response()->json(['success' => false, 'message' => $e], 500);
+        }
     }
 }
