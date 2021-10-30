1c9feb55a7097e49fd3cf9c28828cc49b4933116
smpny7 <58544849+smpny7@users.noreply.github.com>
2021-10-31 02:05:43 +0900

    Fix: 応募ページのバグ修正

--- app/Http/Controllers/API/Present/PresentController.php
+++ app/Http/Controllers/API/Present/PresentController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\Present;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class PresentController extends Controller
 {
