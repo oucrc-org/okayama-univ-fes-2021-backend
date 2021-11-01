3ffece220252674c03c57717744827c18bfcb2d0
smpny7 <58544849+smpny7@users.noreply.github.com>
2021-11-01 16:18:21 +0900

    Add: プレゼント画像パスを追加

--- database/seeders/PresentTableSeeder.php
+++ database/seeders/PresentTableSeeder.php
@@ -18,7 +18,7 @@ class PresentTableSeeder extends Seeder
         DB::table('presents')->insert([
             [
                 'name' => 'Nintendo Switch 本体',
-                'image_path' => '',
+                'image_path' => '/presents/1',
                 'required_stamps' => '8',
                 'stock' => '1',
                 'created_at' => Carbon::now(),
@@ -26,7 +26,7 @@ class PresentTableSeeder extends Seeder
             ],
             [
                 'name' => 'JBL WAVE100 TWS 完全ワイヤレスイヤホン',
-                'image_path' => '',
+                'image_path' => '/presents/2',
                 'required_stamps' => '7',
                 'stock' => '1',
                 'created_at' => Carbon::now(),
@@ -34,7 +34,7 @@ class PresentTableSeeder extends Seeder
             ],
             [
                 'name' => 'HAGOOGI ハンドブレンダー',
-                'image_path' => '',
+                'image_path' => '/presents/3',
                 'required_stamps' => '5',
                 'stock' => '1',
                 'created_at' => Carbon::now(),
@@ -42,7 +42,7 @@ class PresentTableSeeder extends Seeder
             ],
             [
                 'name' => 'DVDプレーヤー DVP-506',
-                'image_path' => '',
+                'image_path' => '/presents/4',
                 'required_stamps' => '6',
                 'stock' => '1',
                 'created_at' => Carbon::now(),
@@ -50,7 +50,7 @@ class PresentTableSeeder extends Seeder
             ],
             [
                 'name' => '加湿器 HU-C1A',
-                'image_path' => '',
+                'image_path' => '/presents/5',
                 'required_stamps' => '6',
                 'stock' => '1',
                 'created_at' => Carbon::now(),
@@ -58,7 +58,7 @@ class PresentTableSeeder extends Seeder
             ],
             [
                 'name' => '岡大農場　ジュレ5個セット',
-                'image_path' => '',
+                'image_path' => '/presents/6',
                 'required_stamps' => '6',
                 'stock' => '1',
                 'created_at' => Carbon::now(),
@@ -66,7 +66,7 @@ class PresentTableSeeder extends Seeder
             ],
             [
                 'name' => '岡大農場　ぷりん　6個セット',
-                'image_path' => '',
+                'image_path' => '/presents/7',
                 'required_stamps' => '6',
                 'stock' => '1',
                 'created_at' => Carbon::now(),
@@ -74,7 +74,7 @@ class PresentTableSeeder extends Seeder
             ],
             [
                 'name' => 'バッファロー USBメモリ 64GB',
-                'image_path' => '',
+                'image_path' => '/presents/8',
                 'required_stamps' => '5',
                 'stock' => '2',
                 'created_at' => Carbon::now(),
@@ -82,7 +82,7 @@ class PresentTableSeeder extends Seeder
             ],
             [
                 'name' => 'スターバックス カード1000円分',
-                'image_path' => '',
+                'image_path' => '/presents/9',
                 'required_stamps' => '5',
                 'stock' => '2',
                 'created_at' => Carbon::now(),
@@ -90,7 +90,7 @@ class PresentTableSeeder extends Seeder
             ],
             [
                 'name' => '岡大 マグカップ',
-                'image_path' => '',
+                'image_path' => '/presents/10',
                 'required_stamps' => '5',
                 'stock' => '2',
                 'created_at' => Carbon::now(),
@@ -98,7 +98,7 @@ class PresentTableSeeder extends Seeder
             ],
             [
                 'name' => '岡大 マフラータオル',
-                'image_path' => '',
+                'image_path' => '/presents/11',
                 'required_stamps' => '5',
                 'stock' => '2',
                 'created_at' => Carbon::now(),
@@ -106,7 +106,7 @@ class PresentTableSeeder extends Seeder
             ],
             [
                 'name' => 'いんでいら えびめしの素 1本',
-                'image_path' => '',
+                'image_path' => '/presents/12',
                 'required_stamps' => '4',
                 'stock' => '4',
                 'created_at' => Carbon::now(),
@@ -114,7 +114,7 @@ class PresentTableSeeder extends Seeder
             ],
             [
                 'name' => '岡大 タオルハンカチ',
-                'image_path' => '',
+                'image_path' => '/presents/13',
                 'required_stamps' => '4',
                 'stock' => '4',
                 'created_at' => Carbon::now(),
@@ -122,7 +122,7 @@ class PresentTableSeeder extends Seeder
             ],
             [
                 'name' => '岡大 きびだんご',
-                'image_path' => '',
+                'image_path' => '/presents/14',
                 'required_stamps' => '4',
                 'stock' => '4',
                 'created_at' => Carbon::now(),
@@ -130,7 +130,7 @@ class PresentTableSeeder extends Seeder
             ],
             [
                 'name' => '図書カード 1000円分',
-                'image_path' => '',
+                'image_path' => '/presents/15',
                 'required_stamps' => '4',
                 'stock' => '4',
                 'created_at' => Carbon::now(),
@@ -138,7 +138,7 @@ class PresentTableSeeder extends Seeder
             ],
             [
                 'name' => '岡大お守り & いーちょメモ帳セット',
-                'image_path' => '',
+                'image_path' => '/presents/16',
                 'required_stamps' => '1',
                 'stock' => '50',
                 'created_at' => Carbon::now(),
