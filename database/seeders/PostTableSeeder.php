ec553c99b611f33eea31cae9a226c1ed3eb4a68b
smpny7 <58544849+smpny7@users.noreply.github.com>
2021-11-04 22:53:36 +0900

    Update: ツイート Seeder 作成

--- database/seeders/PostTableSeeder.php
+++ database/seeders/PostTableSeeder.php
@@ -17,21 +17,29 @@ class PostTableSeeder extends Seeder
      */
     public function run()
     {
-        for ($i = 1; $i <= 20; $i++) {
-            $present_id = rand(1, 16);
-            $present = Present::query()->find($present_id);
-            $apply[] = [
-                'id' => $i,
-                'twitter_id' => 'tweet',
-                'avatar_url' => 'avatar',
-                'display_name' => Str::random(),
-                'comment' => Str::random(),
-                'tweet_url' => Str::random(),
+        DB::table('posts')->insert([
+            [
+                'id' => '1451910148679634948',
+                'twitter_id' => 'chorlotusokadai',
+                'avatar_url' => 'https://pbs.twimg.com/profile_images/444888057414565888/kGBf4Q_F_400x400.jpeg',
+                'display_name' => '岡山大学男声合唱団コール・ロータス',
+                'comment' => '【岡山大学祭2021】今年の学祭は去年同様オンラインで行われます。弊団は、サイト上での部活動紹介、11/16（火）18時からのパフォーマンス発表に出演します。優秀団体には景品が贈られるそうなので、応援よろしくお願いいたします。ご確認のほどよろしくお願い申し上げます。#岡山大学祭2021',
+                'tweet_url' => 'https://twitter.com/chorlotusokadai/status/1451910148679634948',
                 'is_visible' => true,
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ];
-        }
-        DB::table('posts')->insert($apply);
+                'created_at' => new Carbon('2021-10-23'),
+                'updated_at' => new Carbon('2021-10-23'),
+            ],
+            [
+                'id' => '1451933634055659520',
+                'twitter_id' => 'oucrc',
+                'avatar_url' => 'https://pbs.twimg.com/profile_images/1005052861749780485/ISQr7qG0_400x400.jpg',
+                'display_name' => '岡山大学電子計算機研究会',
+                'comment' => '学祭まで残り2週間となりました！電子計算機研究会では部員の作品を展示予定です．是非ご覧ください！ https://oufes2021.com',
+                'tweet_url' => 'https://twitter.com/oucrc/status/1451933634055659520',
+                'is_visible' => true,
+                'created_at' => new Carbon('2021-10-24'),
+                'updated_at' => new Carbon('2021-10-24'),
+            ],
+        ]);
     }
 }
